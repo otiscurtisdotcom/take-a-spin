f7e3b307d4e310528d959071b24ab62c47d7da1c
jack <otiscurtisdotcom@gmail.com>
2025-07-15 15:09:06 +0100

    Rough wheel SVG

--- src/app/wheel-page/wheel/wheel.ts
+++ src/app/wheel-page/wheel/wheel.ts
@@ -1,4 +1,22 @@
-import { Component, input, Signal } from '@angular/core';
+import { Component, computed, input, Signal } from '@angular/core';
+
+interface SegmentData {
+  label: string;
+  color: string;
+}
+
+interface Segment extends SegmentData {
+  rotation: string;
+}
+
+const SEGMENTS_DATA: SegmentData[] = [
+  { label: 'One', color: '#f66' },
+  { label: 'Two', color: '#6f6' },
+  { label: 'Three', color: '#66f' },
+  { label: 'Four', color: '#ff6' },
+  { label: 'Five', color: '#6ff' },
+  { label: 'Six', color: '#f6f' },
+];
 
 @Component({
   selector: 'spinner-app-wheel',
@@ -8,4 +26,14 @@ import { Component, input, Signal } from '@angular/core';
 })
 export class Wheel {
   readonly wheelNumber = input.required<Signal<number | null>>();
+
+  readonly segments: Signal<Segment[]> = computed(() => {
+    return SEGMENTS_DATA.map((segment, i) => {
+      const angle = i * 360 / SEGMENTS_DATA.length;
+      return {
+        ...segment,
+        rotation: `rotate(${angle}, 100, 100)`,
+      };
+    });
+  })
 }
