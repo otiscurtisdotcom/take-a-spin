58f131fb4db00a53a8a2aca8a86a238470df441f
jack <otiscurtisdotcom@gmail.com>
2025-07-16 12:11:36 +0100

    Wheel font tweaks

--- src/app/constants/segments.ts
+++ src/app/constants/segments.ts
@@ -4,12 +4,12 @@ export interface SegmentData {
 }
 
 export const SEGMENTS_DATA: SegmentData[] = [
-  { label: 'One', color: '#c14646' }, // rich coral red
-  { label: 'Two', color: '#3f8c4e' }, // leafy green
-  { label: 'Three', color: '#4566aa' }, // medium slate blue
-  { label: 'Four', color: '#b59a34' }, // golden ochre
-  { label: 'Five', color: '#3c9994' }, // teal blue
-  { label: 'Six', color: '#9a4e97' }, // medium plum
+  { label: '1', color: '#c14646' }, // rich coral red
+  { label: '2', color: '#3f8c4e' }, // leafy green
+  { label: '3', color: '#4566aa' }, // medium slate blue
+  { label: '4', color: '#b59a34' }, // golden ochre
+  { label: '5', color: '#3c9994' }, // teal blue
+  { label: '6', color: '#9a4e97' }, // medium plum
 ]
 
 export const SEGMENTS_CIRCUMFERENCE = 360 / SEGMENTS_DATA.length;
